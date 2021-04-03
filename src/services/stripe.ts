92b57d15b877fa6d63028a40ece2d0492dffe187
Jadson <jadsombruno2016@gmail.com>
2021-04-03 14:29:32 -0300

    Update stripe service

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,6 +1,13 @@
+/**
+ * IMPORTS
+ */
 import Stripe from 'stripe';
 import {version} from '../../package.json';
 
+
+/**
+ * EXPORTS
+ */
 export const stripe = new Stripe(
     process.env.STRIPE_API_KEY,
     {
