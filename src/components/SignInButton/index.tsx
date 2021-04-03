edf159c21080f453aa497e199ee027b45f18e8f5
Jadson <jadsombruno2016@gmail.com>
2021-04-03 14:24:56 -0300

    Update sign in button component

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,31 @@
+/**
+ * IMPORTS
+ */
+import {signIn} from 'next-auth/client';
+import {signOut} from 'next-auth/client';
+import {useSession} from 'next-auth/client';
 import {FaGithub} from 'react-icons/fa';
 import {FiX} from 'react-icons/fi';
 import styles from './styles.module.scss';
 
+
+/**
+ * EXPORTS
+ */
 export function SignInButton ()
 {
-    const isUserLoggedIn = true;
+    // get current session
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
+    // return appropriate component
+    return session ? (
         <button
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361" />
-                Jadson Bruno
+                {session.user.name}
             <FiX
                 color="#737380"
                 className={styles.closeIcon}
@@ -22,6 +35,7 @@ export function SignInButton ()
         <button
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign in with Github
