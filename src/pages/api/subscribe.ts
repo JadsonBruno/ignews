166bb6526e0dce03ad8f21cccca2319fb9f64e6f
Jadson <jadsombruno2016@gmail.com>
2021-04-04 12:36:38 -0300

    Fix stripe URL

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -76,8 +76,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                 ],
                 mode: 'subscription',
                 allow_promotion_codes: true,
-                success_url: `${window.location.origin}/posts`,
-                cancel_url: `${window.location.origin}`
+                success_url: process.env.STRIPE_SUCCESS_URL,
+                cancel_url: process.env.STRIPE_CANCEL_URL
             }
         );
 
